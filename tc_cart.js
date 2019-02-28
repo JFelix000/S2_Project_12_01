05405d826daefd5066b4208dbc7f8ff57881ef1e
Jose Felix <jfelix000@west-mec.org>
2019-02-28 16:18:03 -0700

    Final code ready for review
    Finished the comments as it was the only thing left to do

--- tc_cart.js
+++ tc_cart.js
@@ -19,13 +19,15 @@ var cartHTML = "<table> <tr> <th>Item</th><th>Description</th><th>Price</th><th>
 //itemcost
 
 for (var i = 0; i < item.length; i++) {
+    //this part of the code makes the tr have a different item each time by using the array selector with the i changing because of the for loop.
     cartHTML += "<tr> <td><img src='tc_" + item[i] +  ".png' alt='item' /></td>";
+    //all of the itemdescription, itemprice, and itemqty to change with the for loop.
     cartHTML += "<td>" + itemDescription[i] +  "</td> <td>$" + itemPrice[i] + "</td> <td>" + itemQty[i] + "</td>";
     var itemCost = itemPrice[i] * itemQty[i];
     cartHTML += "<td>$" + itemCost + "</td></tr>";
     orderTotal = orderTotal + itemCost;
 }
-
+//this code adds the tr where ordertotal is calculated by var itemCost.
 cartHTML += "<tr> <td colspan='4'>Subtotal</td> <td>$" + orderTotal +"</td> </tr> </table>"; 
 
 document.getElementById("cart").innerHTML = cartHTML;
